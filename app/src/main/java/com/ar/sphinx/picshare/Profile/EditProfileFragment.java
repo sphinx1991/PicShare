dca60f463bb71234269eddac77c70c9f59370863
sPhinx <ankit.rajput@instavans.com>
2018-03-15 00:44:20 +0530

    feat - Universal image loader added and profile image set

--- app/src/main/java/com/ar/sphinx/picshare/Profile/EditProfileFragment.java
+++ app/src/main/java/com/ar/sphinx/picshare/Profile/EditProfileFragment.java
@@ -1,13 +1,17 @@
 package com.ar.sphinx.picshare.Profile;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.ar.sphinx.picshare.R;
+import com.ar.sphinx.picshare.utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 /**
  * Created by sPhinx on 14/03/18.
@@ -16,11 +20,25 @@ import com.ar.sphinx.picshare.R;
 public class EditProfileFragment extends Fragment {
 
 	private static final String TAG = "EditProfileFragment";
+	private ImageView profileImgView;
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_edit_profile,container,false);
+		profileImgView = view.findViewById(R.id.img_profilepic);
+		initImageLoader();
+		setProfileImage();
 		return view;
+	}
+
+	private void setProfileImage() {
+		String imageUrl = "https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/08/21/20/android-oreo-2.png";
+		UniversalImageLoader.setImage(imageUrl,profileImgView,null,"");
+	}
+
+	private void initImageLoader() {
+		UniversalImageLoader imageLoader = new UniversalImageLoader(getContext());
+		ImageLoader.getInstance().init(imageLoader.getConfig());
 	}
 }
---
+++ app/src/main/java/com/ar/sphinx/picshare/utils/UniversalImageLoader.java
@@ -0,0 +1,85 @@
+package com.ar.sphinx.picshare.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.ar.sphinx.picshare.R;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+/**
+ * Created by sPhinx on 14/03/18.
+ */
+
+public class UniversalImageLoader {
+
+	private static final int default_image = R.drawable.ic_android;
+	private Context context;
+
+	public UniversalImageLoader(Context context){
+		this.context = context;
+	}
+
+	public ImageLoaderConfiguration getConfig(){
+
+		DisplayImageOptions imageOptions = new DisplayImageOptions.Builder()
+				.showImageOnLoading(default_image)
+				.showImageOnFail(default_image)
+				.showImageForEmptyUri(default_image)
+				.cacheOnDisk(true).cacheInMemory(true)
+				.resetViewBeforeLoading(true)
+				.imageScaleType(ImageScaleType.EXACTLY)
+				.displayer(new FadeInBitmapDisplayer(300))
+				.build();
+
+		ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(context)
+				.defaultDisplayImageOptions(imageOptions)
+				.memoryCache(new WeakMemoryCache())
+				.diskCacheSize(100*1024*1024)
+				.build();
+		return configuration;
+	}
+
+	public static void setImage(String imgUrl, ImageView imageView, final ProgressBar progressBar,String append){
+		ImageLoader imageLoader = ImageLoader.getInstance();
+		imageLoader.displayImage(append + imgUrl, imageView, new ImageLoadingListener() { //callback number to inform
+																							  //when image loads/fail and what to do
+			@Override
+			public void onLoadingStarted(String imageUri, View view) {
+				if(progressBar!=null){
+					progressBar.setVisibility(View.VISIBLE);
+				}
+			}
+
+			@Override
+			public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+				if(progressBar!=null){
+					progressBar.setVisibility(View.GONE);
+				}
+			}
+
+			@Override
+			public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+				if(progressBar!=null){
+					progressBar.setVisibility(View.GONE);
+				}
+			}
+
+			@Override
+			public void onLoadingCancelled(String imageUri, View view) {
+				if(progressBar!=null){
+					progressBar.setVisibility(View.GONE );
+				}
+			}
+		});
+	}
+}
