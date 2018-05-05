25f0f5caa246e6d380a5e9aca7c1fe74904e4c5b
sPhinx <ankit.rajput@instavans.com>
2018-05-05 16:49:44 +0530

    feat: login and register screens created

---
+++ app/src/main/java/com/ar/sphinx/picshare/Login/RegisterActivity.java
@@ -0,0 +1,37 @@
+package com.ar.sphinx.picshare.Login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ar.sphinx.picshare.R;
+import com.ar.sphinx.picshare.utils.AppUtils;
+import com.ar.sphinx.picshare.utils.BottomNavViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class RegisterActivity extends AppCompatActivity {
+
+	private static final String TAG = "RegisterActivity";
+	private static final int ACTIVITY_NUM = 3;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		Log.d(TAG, "onCreate: Running ");
+
+		setupBottomNavView();
+	}
+
+	private void setupBottomNavView() {
+		BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottom_nav_bar);
+		BottomNavViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+		BottomNavViewHelper.enableNavigation(this,bottomNavigationViewEx);
+		Menu menu = bottomNavigationViewEx.getMenu();
+		MenuItem item = menu.getItem(ACTIVITY_NUM);
+		item.setChecked(true);
+		AppUtils.LogMsgUtil(TAG,"set up bottom nav view");
+	}
+}
